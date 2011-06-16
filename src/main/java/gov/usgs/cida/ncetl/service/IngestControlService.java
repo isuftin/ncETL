0693506e18e668cbc519537a7143e4be27c41945
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-06-16 12:39:50 +0000

    Added default serialVersionUID
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/dcpt/trunk@15452 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- src/main/java/gov/usgs/cida/ncetl/service/IngestControlService.java
+++ src/main/java/gov/usgs/cida/ncetl/service/IngestControlService.java
@@ -19,6 +19,7 @@ import javax.servlet.http.HttpServletRequest;
  * @author jwalker
  */
 public class IngestControlService extends WebService {
+    private static final long serialVersionUID = 1L;
 
     public IngestControlService() {
         this.enableCaching = false;
