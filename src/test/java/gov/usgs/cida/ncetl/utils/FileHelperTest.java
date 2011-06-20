9a5ddb69e5c3308ebc05da8bf62e6a4f24c87ff9
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-06-20 13:08:07 +0000

    Created tests for FIleHelper class
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/dcpt/trunk@15549 622dc1fb-2224-0410-ab72-dc85a3975e1a

---
+++ src/test/java/gov/usgs/cida/ncetl/utils/FileHelperTest.java
@@ -0,0 +1,104 @@
+package gov.usgs.cida.ncetl.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+/**
+ *
+ * @author isuftin
+ */
+public class FileHelperTest {
+    
+    private static final String tmpDir = System.getProperty("java.io.tmpdir") + File.separator + "test_delete_me" + File.separator;
+    
+    public FileHelperTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        FileUtils.deleteDirectory(new File(tmpDir));
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+        FileUtils.deleteDirectory(new File(tmpDir));
+    }
+    
+    @Before
+    public void beforeTest() throws Exception {
+        FileUtils.deleteDirectory(new File(tmpDir));
+    }
+    
+    @After
+    public void afterTest() throws Exception {
+        FileUtils.deleteDirectory(new File(tmpDir));
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void getBaseDirectory() {
+        String test = FileHelper.getBaseDirectory();
+        assertThat(test, is(notNullValue()));
+    }
+    
+    @Test
+    public void getDatabaseDirectory() {
+        String test = FileHelper.getDatabaseDirectory();
+        assertThat(test, is(notNullValue()));
+    }
+    
+    @Test
+    public void createDirectory() {
+        File testFile = new File(tmpDir);
+        try {
+            FileHelper.createDirectory(testFile);
+        } catch (IOException ex) {
+            Logger.getLogger(FileHelperTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        assertThat(testFile.exists(), is(true));
+    }      
+    
+    /**
+     * This tests to make sure that createDirectory does not touch
+     * the existing directory if it already exists
+     */
+    @Test
+    public void createExistingDirectory() {
+        File testFile = new File(tmpDir);
+        File testFile2 = new File(tmpDir);
+        
+        try {
+            FileHelper.createDirectory(testFile);
+        } catch (IOException ex) {
+            Logger.getLogger(FileHelperTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        long lastModified = testFile.lastModified();
+        
+        try {
+            FileHelper.createDirectory(testFile2);
+        } catch (IOException ex) {
+            Logger.getLogger(FileHelperTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        long lastModified2 = testFile2.lastModified();
+        
+        assertThat(lastModified, is(equalTo(lastModified2)));               
+        
+    }     
+}
