f385e7352a24c2925c8415b63b3abc7fc385d45f
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-06-24 11:47:10 +0000

    All tests but one are passing
    Fault is at line 90
    
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/dcpt/trunk@15683 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- src/test/java/thredds/catalog/InvDatasetBuilderTest.java
+++ src/test/java/thredds/catalog/InvDatasetBuilderTest.java
@@ -3,6 +3,7 @@ package thredds.catalog;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import ucar.nc2.constants.FeatureType;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 /**
@@ -23,15 +24,69 @@ public class InvDatasetBuilderTest {
     }
 
     @Test
-    public void testSomeMethod() {
-        assertThat(true, is(true));
-//        InvDatasetBuilder test = new InvDatasetBuilder("test");
-//        test.id("testId")
-//            .collectionType("testCollectionType")
-//            .contributor("testName", "testRole");
-//        assertThat(test.getInternalInvDataset().getID(), is(equalTo("testId")));
-//        assertThat(test.getInternalInvDataset().getCollectionType(), is(notNullValue()));
-//        assertThat(test.getInternalInvDataset().getContributors().get(0).getName(), is(equalTo("testName")));
-//        assertThat(test.getInternalInvDataset().getContributors().get(0).getRole(), is(equalTo("testRole")));
+    public void testInvDatasetBuilder() {
+        InvDatasetBuilder testBuilder = new InvDatasetBuilder("testName", "testID");
+        InvDataset test = testBuilder.authorityName("testAuthorityName")
+                .collectionType("testCollectionType")
+                .contributor("testContributorName", "testContributorRole")
+                .creator("testCreatorName", "testCreatorURL", "testCreatorEmail", "testCreatorVocab")
+                .dataFormatType("testDataFormatType") 
+                .dataType(FeatureType.ANY.name()) 
+                .documentation("testDocumentationType", "testDocumentationText")
+                .xlinkDocumentation("testXlinkDocumentationHref", "testXlinkDocumentationTitle") 
+                .keyword("testKeyword", "testVocab") 
+                .project("testProject", "testVocab")
+                .property("testKey", "testValue")
+                .publisher("testPublisherName", "testPublisherURL", "testPublisherEmail", "testPublisherVocab") 
+                .service("testServiceName")
+                .build();
+        
+        assertThat(test.authorityName, is(equalTo("testAuthorityName")));
+        
+        assertThat(test.collectionType.toString(), is(equalTo("testCollectionType")));
+        
+        assertThat(test.contributors, is(notNullValue()));
+        assertThat(test.contributors.size(), is(equalTo(1)));
+        assertThat(test.contributors.get(0).getName(), is(equalTo("testContributorName")));
+        assertThat(test.contributors.get(0).getRole(), is(equalTo("testContributorRole")));
+        
+        assertThat(test.getCreators(), is(notNullValue()));
+        assertThat(test.getCreators().size(), is(equalTo(1)));
+        assertThat(test.getCreators().get(0).getName(), is(equalTo("testCreatorName")));
+        assertThat(test.getCreators().get(0).getUrl(), is(equalTo("testCreatorURL")));
+        assertThat(test.getCreators().get(0).getEmail(), is(equalTo("testCreatorEmail")));
+        assertThat(test.getCreators().get(0).getVocabulary(), is(equalTo("testCreatorVocab")));
+        
+        assertThat(test.getDataType().name(), is(equalTo("ANY")));
+        
+        assertThat(test.getDocumentation(), is(notNullValue()));
+        assertThat(test.getDocumentation().size(), is(equalTo(2)));
+        assertThat(test.getDocumentation().get(0).getType(), is(equalTo("testDocumentationType")));
+        assertThat(test.getDocumentation().get(0).getInlineContent(), is(equalTo("testDocumentationText")));
+        assertThat(test.getDocumentation().get(1).getXlinkHref(), is(equalTo("testXlinkDocumentationHref")));
+        assertThat(test.getDocumentation().get(1).getXlinkTitle(), is(equalTo("testXlinkDocumentationTitle")));
+        
+        assertThat(test.getKeywords().size(), is(equalTo(1)));
+        assertThat(test.getKeywords().get(0).getText(), is(equalTo("testKeyword")));
+        assertThat(test.getKeywords().get(0).getVocabulary(), is(equalTo("testVocab")));
+        
+        assertThat(test.getProjects(), is(notNullValue()));
+        assertThat(test.getProjects().size(), is(equalTo(1)));
+        assertThat(test.getProjects().get(0).getText(), is(equalTo("testProject")));
+        assertThat(test.getProjects().get(0).getVocabulary(), is(equalTo("testVocab")));
+        
+        assertThat(test.getProperties(), is(notNullValue()));
+        assertThat(test.getProperties().size(), is(equalTo(1)));
+        assertThat(test.getProperties().get(0).getName(), is(equalTo("testKey")));
+        assertThat(test.getProperties().get(0).getValue(), is(equalTo("testValue")));
+        
+        assertThat(test.getPublishers(), is(notNullValue()));
+        assertThat(test.getPublishers().size(), is(equalTo(1)));
+        assertThat(test.getPublishers().get(0).getName(), is(equalTo("testPublisherName")));
+        assertThat(test.getPublishers().get(0).getUrl(), is(equalTo("testPublisherURL")));
+        assertThat(test.getPublishers().get(0).getEmail(), is(equalTo("testPublisherEmail")));
+        assertThat(test.getPublishers().get(0).getVocabulary(), is(equalTo("testPublisherVocab")));
+        
+        //assertThat(test.services.get(0).getName(), is(equalTo("testServiceName"))); 
     }
 }
