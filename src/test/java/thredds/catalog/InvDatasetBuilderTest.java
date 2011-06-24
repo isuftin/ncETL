ac3c90ba6f17ace8a6633a587b8f6e55db3014e6
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-06-24 11:47:04 +0000

    commented temporarily
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/dcpt/trunk@15676 622dc1fb-2224-0410-ab72-dc85a3975e1a

---
+++ src/test/java/thredds/catalog/InvDatasetBuilderTest.java
@@ -0,0 +1,36 @@
+package thredds.catalog;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+/**
+ *
+ * @author Ivan Suftin <isuftin@usgs.gov>
+ */
+public class InvDatasetBuilderTest {
+    
+    public InvDatasetBuilderTest() {
+    }
+
+    @Before
+    public  void setUp() throws Exception {
+    }
+
+    @After
+    public  void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testSomeMethod() {
+//        InvDatasetBuilder test = new InvDatasetBuilder("test");
+//        test.id("testId")
+//            .collectionType("testCollectionType")
+//            .contributor("testName", "testRole");
+//        assertThat(test.getInternalInvDataset().getID(), is(equalTo("testId")));
+//        assertThat(test.getInternalInvDataset().getCollectionType(), is(notNullValue()));
+//        assertThat(test.getInternalInvDataset().getContributors().get(0).getName(), is(equalTo("testName")));
+//        assertThat(test.getInternalInvDataset().getContributors().get(0).getRole(), is(equalTo("testRole")));
+    }
+}
