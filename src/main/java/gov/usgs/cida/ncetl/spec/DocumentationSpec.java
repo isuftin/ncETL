bc7e551b02ca6466bbde87ca54306e4b957469f8
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-07-06 18:56:02 +0000

    Service and Spec added for "DOCUMENTATION" table
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/dcpt/trunk@15916 622dc1fb-2224-0410-ab72-dc85a3975e1a

---
+++ src/main/java/gov/usgs/cida/ncetl/spec/DocumentationSpec.java
@@ -0,0 +1,147 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gov.usgs.cida.ncetl.spec;
+
+import gov.usgs.webservices.jdbc.spec.Spec;
+import gov.usgs.webservices.jdbc.spec.mapping.ColumnMapping;
+import gov.usgs.webservices.jdbc.spec.mapping.SearchMapping;
+import gov.usgs.webservices.jdbc.spec.mapping.WhereClauseType;
+import gov.usgs.webservices.jdbc.util.ServiceUtils;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author Ivan Suftin <isuftin@usgs.gov>
+ */
+public class DocumentationSpec  extends Spec {
+    private static final long serialVersionUID = 1L;
+    
+    private static final String TABLE_NAME = "ingests";
+    private static final String ID = "id";
+    private static final String DOCUMENTATION_TYPE_ID = "documentation_type_id";
+    private static final String DATASET_ID = "dataset_id";
+    private static final String XLINK_HREF = "xlink_href";
+    private static final String XLINK_TITLE = "xlink_title";
+    private static final String TEXT = "text";
+    private static final String INSERTED = "inserted";
+    private static final String UPDATED = "updated";
+    
+    @Override
+    public boolean setupAccess_DELETE() {
+        return true;
+    }
+
+    @Override
+    public boolean setupAccess_INSERT() {
+        return true;
+    }
+
+    @Override
+    public boolean setupAccess_READ() {
+        return true;
+    }
+
+    @Override
+    public boolean setupAccess_UPDATE() {
+        return true;
+    }
+
+    @Override
+    public ColumnMapping[] setupColumnMap() {
+        return new ColumnMapping[] {
+                    new ColumnMapping(ID, ID),
+                    new ColumnMapping(DOCUMENTATION_TYPE_ID, DOCUMENTATION_TYPE_ID),
+                    new ColumnMapping(DATASET_ID, DATASET_ID),
+                    new ColumnMapping(XLINK_HREF, XLINK_HREF),
+                    new ColumnMapping(XLINK_TITLE, XLINK_TITLE),
+                    new ColumnMapping(TEXT, TEXT),
+                    new ColumnMapping(INSERTED, null),
+                    new ColumnMapping(UPDATED, null)
+                };
+    }
+
+    @Override
+    public String setupDocTag() {
+        return "success";
+    }
+
+    @Override
+    public String setupRowTag() {
+        return "data";
+    }
+
+    @Override
+    public SearchMapping[] setupSearchMap() {
+        return new SearchMapping[] {
+            new SearchMapping(ID, ID, null, WhereClauseType.equals, null, null, null),
+            new SearchMapping("s_" + DOCUMENTATION_TYPE_ID, DOCUMENTATION_TYPE_ID, DOCUMENTATION_TYPE_ID, WhereClauseType.equals, null, null, null),
+            new SearchMapping("s_" + DATASET_ID, DATASET_ID, DATASET_ID, WhereClauseType.equals, null, null, null),
+            new SearchMapping("s_" + XLINK_HREF, XLINK_HREF, XLINK_HREF, WhereClauseType.equals, null, null, null),
+            new SearchMapping("s_" + XLINK_TITLE, XLINK_TITLE, XLINK_TITLE, WhereClauseType.equals, null, null, null),
+            new SearchMapping("s_" + TEXT, TEXT, TEXT, WhereClauseType.equals, null, null, null),
+            new SearchMapping("s_" + INSERTED, INSERTED, INSERTED, WhereClauseType.equals, null, null, null),
+            new SearchMapping("s_" + UPDATED, UPDATED, UPDATED, WhereClauseType.equals, null, null, null)
+        };
+    }
+
+    @Override
+    public String setupTableName() {
+        return TABLE_NAME;
+    }
+
+    @Override
+    public ResultSet getUpdatedRows(Connection con) throws SQLException {
+        ResultSet result = null;
+        Spec spec = new IngestControlSpec();
+        Map<String, String[]> params = new HashMap<String, String[]>();
+        params.put("s_" + UPDATED, new String[] {"true"});
+        Spec.loadParameters(spec, params);
+        List<String> names = ServiceUtils.getStringsFromDB(spec, con, ID);
+        
+        params = new HashMap<String, String[]>();
+        params.put(UPDATED, new String[] {"false"});
+        params.put(ID, names.toArray(new String[0]));
+        spec = new IngestControlSpec();
+        Spec.loadParameters(spec, params);
+        Spec.updateRow(spec, con);
+        
+        spec = new IngestControlSpec();
+        params = new HashMap<String, String[]>();
+        params.put(ID, names.toArray(new String[0]));
+        
+        result = Spec.getResultSet(spec, con);
+        return result;
+    }
+
+    @Override
+    public ResultSet getInsertedRows(Connection con) throws SQLException {
+        ResultSet result = null;
+        Spec spec = new IngestControlSpec();
+        Map<String, String[]> params = new HashMap<String, String[]>();
+        params.put("s_" + INSERTED, new String[] {"true"});
+        Spec.loadParameters(spec, params);
+        List<String> names = ServiceUtils.getStringsFromDB(spec, con, ID);
+        
+        params = new HashMap<String, String[]>();
+        params.put(INSERTED, new String[] {"false"});
+        params.put(ID, names.toArray(new String[0]));
+        spec = new IngestControlSpec();
+        Spec.loadParameters(spec, params);
+        Spec.updateRow(spec, con);
+        
+        spec = new IngestControlSpec();
+        params = new HashMap<String, String[]>();
+        params.put(ID, names.toArray(new String[0]));
+        Spec.loadParameters(spec, params);
+        
+        result = Spec.getResultSet(spec, con);
+        return result;
+    }
+}
