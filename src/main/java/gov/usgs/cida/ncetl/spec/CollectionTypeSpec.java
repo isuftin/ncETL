ded3057d92f47e7396ef61e8bbd8687cf3561a35
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-07-06 19:06:46 +0000

    Service and Spec added for "CollectionType" table
    Changed table name in DocumentationSpec to Documentation
    
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/dcpt/trunk@15918 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- src/main/java/gov/usgs/cida/ncetl/spec/CollectionTypeSpec.java
+++ src/main/java/gov/usgs/cida/ncetl/spec/CollectionTypeSpec.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package gov.usgs.cida.ncetl.spec;
 
 import gov.usgs.webservices.jdbc.spec.Spec;
@@ -20,16 +16,12 @@ import java.util.Map;
  *
  * @author Ivan Suftin <isuftin@usgs.gov>
  */
-public class DocumentationSpec  extends Spec {
+public class CollectionTypeSpec extends Spec {
     private static final long serialVersionUID = 1L;
-    
-    private static final String TABLE_NAME = "documentation";
+    //id int, type varchar(32), inserted boolean, updated boolean)
+    private static final String TABLE_NAME = "collection_types";
     private static final String ID = "id";
-    private static final String DOCUMENTATION_TYPE_ID = "documentation_type_id";
-    private static final String DATASET_ID = "dataset_id";
-    private static final String XLINK_HREF = "xlink_href";
-    private static final String XLINK_TITLE = "xlink_title";
-    private static final String TEXT = "text";
+    private static final String TYPE = "type";
     private static final String INSERTED = "inserted";
     private static final String UPDATED = "updated";
     
@@ -57,11 +49,7 @@ public class DocumentationSpec  extends Spec {
     public ColumnMapping[] setupColumnMap() {
         return new ColumnMapping[] {
                     new ColumnMapping(ID, ID),
-                    new ColumnMapping(DOCUMENTATION_TYPE_ID, DOCUMENTATION_TYPE_ID),
-                    new ColumnMapping(DATASET_ID, DATASET_ID),
-                    new ColumnMapping(XLINK_HREF, XLINK_HREF),
-                    new ColumnMapping(XLINK_TITLE, XLINK_TITLE),
-                    new ColumnMapping(TEXT, TEXT),
+                    new ColumnMapping(TYPE, TYPE),
                     new ColumnMapping(INSERTED, null),
                     new ColumnMapping(UPDATED, null)
                 };
@@ -81,11 +69,7 @@ public class DocumentationSpec  extends Spec {
     public SearchMapping[] setupSearchMap() {
         return new SearchMapping[] {
             new SearchMapping(ID, ID, null, WhereClauseType.equals, null, null, null),
-            new SearchMapping("s_" + DOCUMENTATION_TYPE_ID, DOCUMENTATION_TYPE_ID, DOCUMENTATION_TYPE_ID, WhereClauseType.equals, null, null, null),
-            new SearchMapping("s_" + DATASET_ID, DATASET_ID, DATASET_ID, WhereClauseType.equals, null, null, null),
-            new SearchMapping("s_" + XLINK_HREF, XLINK_HREF, XLINK_HREF, WhereClauseType.equals, null, null, null),
-            new SearchMapping("s_" + XLINK_TITLE, XLINK_TITLE, XLINK_TITLE, WhereClauseType.equals, null, null, null),
-            new SearchMapping("s_" + TEXT, TEXT, TEXT, WhereClauseType.equals, null, null, null),
+            new SearchMapping("s_" + TYPE, TYPE, TYPE, WhereClauseType.equals, null, null, null),
             new SearchMapping("s_" + INSERTED, INSERTED, INSERTED, WhereClauseType.equals, null, null, null),
             new SearchMapping("s_" + UPDATED, UPDATED, UPDATED, WhereClauseType.equals, null, null, null)
         };
@@ -144,4 +128,5 @@ public class DocumentationSpec  extends Spec {
         result = Spec.getResultSet(spec, con);
         return result;
     }
+    
 }
