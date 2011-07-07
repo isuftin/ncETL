b4f3c9e336758fade17d381ba92dcb149909c941
jiwalker <jiwalker@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-07-07 23:41:24 +0000

    database tables
    * added serviceType
    * removed plural languages
    * added vocabulary
    * added keyword + join
    * added contributor + join
    * added creator + join
    
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/dcpt/trunk@15966 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- src/main/java/gov/usgs/cida/ncetl/utils/DatabaseUtil.java
+++ src/main/java/gov/usgs/cida/ncetl/utils/DatabaseUtil.java
@@ -44,37 +44,52 @@ public final class DatabaseUtil {
         CREATE_MAP = Maps.newLinkedHashMap();
         
         // Lookup Tables
-        CREATE_MAP.put("COLLECTION_TYPES", 
-                       "CREATE TABLE collection_types (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), type varchar(32), inserted boolean, updated boolean, PRIMARY KEY (id))");
-        CREATE_MAP.put("DATA_TYPES", 
-                       "CREATE TABLE data_types (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), type varchar(32), inserted boolean, updated boolean, PRIMARY KEY (id))");
+        CREATE_MAP.put("COLLECTION_TYPE", 
+                       "CREATE TABLE collection_type (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), type varchar(32), inserted boolean, updated boolean, PRIMARY KEY (id))");
+        CREATE_MAP.put("DATA_TYPE", 
+                       "CREATE TABLE data_type (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), type varchar(32), inserted boolean, updated boolean, PRIMARY KEY (id))");
         CREATE_MAP.put("DATA_FORMAT", 
                        "CREATE TABLE data_format (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), type varchar(32), inserted boolean, updated boolean, PRIMARY KEY (id))");
-        CREATE_MAP.put("DOCUMENTATION_TYPES", 
-                       "CREATE TABLE documentation_types (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), type varchar(32), inserted boolean, updated boolean, PRIMARY KEY (id))");
+        CREATE_MAP.put("DOCUMENTATION_TYPE", 
+                       "CREATE TABLE documentation_type (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), type varchar(32), inserted boolean, updated boolean, PRIMARY KEY (id))");
+        CREATE_MAP.put("SERVICE_TYPE", 
+                       "CREATE TABLE service_type (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), type varchar(32), inserted boolean, updated boolean, PRIMARY KEY (id))");
+        CREATE_MAP.put("CONTROLLED_VOCABULARY", 
+                       "CREATE TABLE controlled_vocabulary (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), vocab varchar(32), inserted boolean, updated boolean, PRIMARY KEY (id))");
         
         CREATE_MAP.put("GLOBAL_CONFIG",
                       "CREATE TABLE global_config (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), base_dir varchar(512), thredds_dir varchar(512), PRIMARY KEY (id))");
-        CREATE_MAP.put("CATALOGS", 
-                       "CREATE TABLE catalogs (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), catalog_id INT CONSTRAINT CATALOGS1_FK REFERENCES catalogs, location varchar(512), name varchar(64), expires date, version varchar(8), inserted boolean, updated boolean, PRIMARY KEY (id))"); //TODO- service, property and dataset can be subtables
-        CREATE_MAP.put("INGESTS",
-                      "CREATE TABLE ingests (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), catalog_id INT CONSTRAINT CATALOGS2_FK REFERENCES catalogs, name varchar(128), ftpLocation varchar(512), rescanEvery bigint, fileRegex varchar(64), successDate date, successTime time, username varchar(64), password varchar(64), active boolean, inserted boolean, updated boolean, PRIMARY KEY (id))");
-        CREATE_MAP.put("DATASETS", 
-                       "CREATE TABLE datasets (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), catalog_id INT CONSTRAINT CATALOGS3_FK REFERENCES catalogs, collection_type_id INT CONSTRAINT COLLECTION1_FK REFERENCES collection_types, data_type_id INT CONSTRAINT DATATYPE_FK REFERENCES data_types, name varchar(64), ncid varchar(128), authority varchar(64), inserted boolean, updated boolean, PRIMARY KEY (id))");
-        CREATE_MAP.put("SERVICES", 
-                       "CREATE TABLE services (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), catalog_id INT CONSTRAINT CATALOGS4_FK REFERENCES catalogs, service_id INT CONSTRAINT SERVICE1_FK REFERENCES services,  /*service_type_id INT CONSTRAINT SERVICETYPE_FK REFERENCES service_types,*/ name varchar(64), base varchar(32),  description varchar(512), suffix varchar(32), inserted boolean, updated boolean, PRIMARY KEY (id))");
+        CREATE_MAP.put("CATALOG", 
+                       "CREATE TABLE catalog (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), location varchar(512), name varchar(64), expires date, version varchar(8), inserted boolean, updated boolean, PRIMARY KEY (id))"); //TODO- service, property and dataset can be subtables
+        CREATE_MAP.put("INGEST",
+                      "CREATE TABLE ingest (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), catalog_id INT CONSTRAINT CATALOG2_FK REFERENCES catalog, name varchar(128), ftpLocation varchar(512), rescanEvery bigint, fileRegex varchar(64), successDate date, successTime time, username varchar(64), password varchar(64), active boolean, inserted boolean, updated boolean, PRIMARY KEY (id))");
+        CREATE_MAP.put("DATASET", 
+                       "CREATE TABLE dataset (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), catalog_id INT CONSTRAINT CATALOG3_FK REFERENCES catalog, collection_type_id INT CONSTRAINT COLLECTION1_FK REFERENCES collection_type, data_type_id INT CONSTRAINT DATATYPE_FK REFERENCES data_type, name varchar(64), ncid varchar(128), authority varchar(64), inserted boolean, updated boolean, PRIMARY KEY (id))");
+        CREATE_MAP.put("SERVICE", 
+                       "CREATE TABLE service (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), catalog_id INT CONSTRAINT CATALOG4_FK REFERENCES catalog, service_id INT CONSTRAINT SERVICE1_FK REFERENCES service,  service_type_id INT CONSTRAINT SERVICETYPE_FK REFERENCES service_type, name varchar(64), base varchar(32),  description varchar(512), suffix varchar(32), inserted boolean, updated boolean, PRIMARY KEY (id))");
         CREATE_MAP.put("ACCESS", 
-                       "CREATE TABLE access (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), dataset_id INT CONSTRAINT DATASET1_FK REFERENCES datasets, service_id INT CONSTRAINT SERVICE2_FK REFERENCES services, dataformat_id INT CONSTRAINT DATAFORMAT_FK REFERENCES data_format, url_path varchar(512), inserted boolean, updated boolean, PRIMARY KEY (id))"); //TODO - Can have data_size
+                       "CREATE TABLE access (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), dataset_id INT CONSTRAINT DATASET1_FK REFERENCES dataset, service_id INT CONSTRAINT SERVICE2_FK REFERENCES service, dataformat_id INT CONSTRAINT DATAFORMAT_FK REFERENCES data_format, url_path varchar(512), inserted boolean, updated boolean, PRIMARY KEY (id))"); //TODO - Can have data_size
         
         CREATE_MAP.put("DOCUMENTATION", 
-                       "CREATE TABLE documentation (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), dataset_id INT CONSTRAINT DATASET2_FK REFERENCES datasets, documentation_type_id INT CONSTRAINT DOCTYPE_FK REFERENCES documentation_types, xlink_href varchar(256), xlink_title varchar(256), text varchar(1024), inserted boolean, updated boolean, PRIMARY KEY (id))");
+                       "CREATE TABLE documentation (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), dataset_id INT CONSTRAINT DATASET2_FK REFERENCES dataset, documentation_type_id INT CONSTRAINT DOCTYPE_FK REFERENCES documentation_type, xlink_href varchar(256), xlink_title varchar(256), text varchar(1024), inserted boolean, updated boolean, PRIMARY KEY (id))");
         CREATE_MAP.put("PROPERTY", 
-                       "CREATE TABLE property (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), dataset_id INT CONSTRAINT DATASET3_FK REFERENCES datasets, name varchar(128), value varchar(512), inserted boolean, updated boolean, PRIMARY KEY (id))");
+                       "CREATE TABLE property (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), dataset_id INT CONSTRAINT DATASET3_FK REFERENCES dataset, name varchar(128), value varchar(512), inserted boolean, updated boolean, PRIMARY KEY (id))");
+        CREATE_MAP.put("KEYWORD", 
+                       "CREATE TABLE keyword (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), value varchar(64), controlled_vocabulary_id INT CONSTRAINT VOCAB1_FK REFERENCES controlled_vocabulary, inserted boolean, updated boolean, PRIMARY KEY (id))");
+        CREATE_MAP.put("CONTRIBUTOR", 
+                       "CREATE TABLE contributor (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), role varchar(64), text varchar(256), inserted boolean, updated boolean, PRIMARY KEY (id))");
+        CREATE_MAP.put("CREATOR", 
+                       "CREATE TABLE creator (id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), name varchar(256), controlled_vocabulary_id INT CONSTRAINT VOCAB1_FK REFERENCES controlled_vocabulary, contact_url varchar(512), contact_email varchar(256), inserted boolean, updated boolean, PRIMARY KEY (id))");
         
-        // TODO- Create service type table
-        // Check this for completeness
-        //CREATE_MAP.put("KEYWORDS",
-        //               "CREATE TABLE keywords (id int, dataset_id int, value varchar(64), vocab varchar(64))");
+        // Join Tables?
+        // helps to manage many-to-one relationships
+        CREATE_MAP.put("KEYWORD_JOIN", 
+                       "CREATE TABLE keyword_join (dataset_id INT CONSTRAINT DATASET4_FK REFERENCES dataset, keyword_id INT CONSTRAINT KEYWORD_FK REFERENCES keyword, inserted boolean, updated boolean)");
+        CREATE_MAP.put("CONTRIBUTOR_JOIN", 
+                       "CREATE TABLE contributor_join (dataset_id INT CONSTRAINT DATASET5_FK REFERENCES dataset, contributor_id INT CONSTRAINT CONTRIB_FK REFERENCES contributor, inserted boolean, updated boolean)");
+        CREATE_MAP.put("CREATOR_JOIN", 
+                       "CREATE TABLE creator_join (dataset_id INT CONSTRAINT DATASET6_FK REFERENCES dataset, creator_id INT CONSTRAINT CREATOR_FK REFERENCES creator, inserted boolean, updated boolean)");
+        
     }
 
     public static void setupDatabase() throws SQLException, NamingException,
