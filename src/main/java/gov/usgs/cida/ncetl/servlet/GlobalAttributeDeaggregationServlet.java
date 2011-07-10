365deb3fe257fc8ec07a84263df3cfbcb682df27
jiwalker <jiwalker@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-07-10 23:12:44 +0000

    minor issues are fixed, seems to work
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/dcpt/trunk@16018 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- src/main/java/gov/usgs/cida/ncetl/servlet/GlobalAttributeDeaggregationServlet.java
+++ src/main/java/gov/usgs/cida/ncetl/servlet/GlobalAttributeDeaggregationServlet.java
@@ -2,8 +2,11 @@ package gov.usgs.cida.ncetl.servlet;
 
 import gov.usgs.cida.ncetl.utils.NcMLUtil;
 import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -30,40 +33,59 @@ public class GlobalAttributeDeaggregationServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("application/xml;charset=UTF-8");
         OutputStream out = response.getOutputStream();
+
+        String directory = request.getParameter("directory");
+        boolean recurse = ("true".equalsIgnoreCase(request.getParameter("recurse")));
+        final String regex = request.getParameter("regex");
+        
+        FileFilter filter = new FileFilter() {
+            Pattern pattern = null;
+
+            @Override
+            public boolean accept(File pathname) {
+                pattern = (null != regex) ? Pattern.compile(regex) : Pattern.compile(".*");
+                Matcher matcher = pattern.matcher(pathname.getName());
+                return matcher.matches();
+            }
+        };
+
         WrapperNetcdfFile globals = new WrapperNetcdfFile();
-        Group rootGroup = new Group(globals, null, "Aggregated global attributes");
+        Group rootGroup = new Group(globals, null,
+                                    "Aggregated global attributes");
         globals.addGroup(null, rootGroup);
         try {
-            String directory = request.getParameter("directory");
-            boolean recurse = ("true".equalsIgnoreCase(request.getParameter("recurse")));
+
             File file = new File(directory);
-            addDirectoryContents(file, globals, recurse);
-            globals.writeNcML(out);
+            addDirectoryContents(file, globals, rootGroup, recurse, filter);
+            globals.writeNcML(out, null);
         }
-        finally {            
+        finally {
             IOUtils.closeQuietly(out);
         }
     }
-    
-    private void addDirectoryContents(File directory, WrapperNetcdfFile ncml, boolean recurse) throws IOException {
+
+    private void addDirectoryContents(File directory, WrapperNetcdfFile ncml,
+                                      Group metaGroup, boolean recurse, FileFilter filter) throws
+            IOException {
         if (directory.isDirectory()) {
-            for (File file : directory.listFiles()) {
+            for (File file : directory.listFiles(filter)) {
                 if (file.isDirectory() && recurse) {
-                    addDirectoryContents(directory, ncml, recurse);
+                    addDirectoryContents(directory, ncml, metaGroup, recurse, filter);
                 }
                 else {
-                    addFileContents(file, ncml);
+                    addFileContents(file, ncml, metaGroup);
                 }
             }
         }
         else {
-            addFileContents(directory, ncml);
+            addFileContents(directory, ncml, metaGroup);
         }
     }
-    
-    private void addFileContents(File file, WrapperNetcdfFile globals) throws IOException {
+
+    private void addFileContents(File file, WrapperNetcdfFile globals,
+                                 Group metaGroup) throws IOException {
         Group globalGroup = NcMLUtil.globalAttributesToMeta(file, globals);
-        globals.addGroup(globals.getRootGroup(), globalGroup);
+        globals.addGroup(metaGroup, globalGroup);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -103,6 +125,4 @@ public class GlobalAttributeDeaggregationServlet extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
-
 }
