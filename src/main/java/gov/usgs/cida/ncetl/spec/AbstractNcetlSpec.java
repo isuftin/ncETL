a0d96fc6b7d2f250307f65aa0214b5f4911333a5
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-07-11 20:12:29 +0000

    Fixed imports
    deleted Datasethelper and SyncWithDatabase
    
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/dcpt/trunk@16054 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- src/main/java/gov/usgs/cida/ncetl/spec/AbstractNcetlSpec.java
+++ src/main/java/gov/usgs/cida/ncetl/spec/AbstractNcetlSpec.java
@@ -10,8 +10,6 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
