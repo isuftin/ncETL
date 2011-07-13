261c054a2d0b48177f53b949b049f179b918f14d
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-07-13 00:24:58 +0000

    Fixed ID searching
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/dcpt/trunk@16142 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- src/main/java/gov/usgs/cida/ncetl/spec/UpDownTypeSpec.java
+++ src/main/java/gov/usgs/cida/ncetl/spec/UpDownTypeSpec.java
@@ -59,7 +59,7 @@ public class UpDownTypeSpec extends AbstractNcetlSpec {
     public static String lookup(int id, Connection con) throws SQLException {
         Spec spec = new UpDownTypeSpec();
         Map<String, String[]> params = Maps.newHashMap();
-        params.put("s_" + ID, new String[] { "" + id });
+        params.put(ID, new String[] { "" + id });
         Spec.loadParameters(spec, params);
         ResultSet rs = Spec.getResultSet(spec, con);
         String type = null;
