8ab83a6bfe77110836f58af0c0d4fa71d3d74e1b
isuftin <isuftin@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-07-15 17:40:20 +0000

    Removed unused private fields
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/dcpt/trunk@16220 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- src/main/java/thredds/catalog2/simpleImpl/PropertyImplBuilder.java
+++ src/main/java/thredds/catalog2/simpleImpl/PropertyImplBuilder.java
@@ -5,14 +5,10 @@ package thredds.catalog2.simpleImpl;
  * @author isuftin
  */
 public class PropertyImplBuilder {
-    private String name;
-    private String value;
     private boolean built = false;
     private PropertyImpl propertyImpl;
     
     public PropertyImplBuilder(final String name, final String value) {
-        this.name = name;
-        this.value = value;
         propertyImpl = new PropertyImpl(name, value);
     }
     
